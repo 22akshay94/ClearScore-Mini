7f0f0655b721c975f6752f8ca05c31cf3ee8becd
Akshay <akshay.yerneni@hotmail.com>
2022-01-21 03:32:36 +0000

    Initial commit: Project Start

---
+++ ClearScore-Mini/ClearScore-Mini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClearScore-Mini
+//
+//  Created by Akshay Yerneni on 21/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
